e0099ed8e5211dd93c927c7c96b31abca4e2f707
bluesteven <51227406+bluesteven@users.noreply.github.com>
2023-10-31 11:24:59 +0800

    Update unetmobv2.py

--- maskay/library/unetmobv2.py
+++ maskay/library/unetmobv2.py
@@ -12,10 +12,8 @@ class UnetMobV2(Module):
         self.model = model_setup()
 
     def forward(self, x):
-        # 配置日志输出到文件
-        logging.basicConfig(filename='/content/log.txt', level=logging.DEBUG)
-        # 输出到日志文件
-        logging.debug('Variable shape: %s', x.shape)
+        with open('/content/log.txt', 'a') as file:
+            file.write(str(x.shape))
         return self.model(x)
 
     
